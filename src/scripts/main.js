048c7d4081faa15024f87a0ea13c1c7c3bc7698b
Boris Mund <boris17527684@gmail.com>
2025-11-03 00:14:26 +0300

    fix probl

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,10 +9,11 @@ for (const ch of popul) {
   const str = ch.textContent.replace(/,/g, '');
   const num = Number(str);
 
-  sum += num;
-
-  average = Math.floor(sum / popul.length);
+  if (!isNaN(num)) {
+    sum += num;
+  }
 }
+average = Math.floor(sum / popul.length);
 
 document.querySelector('.total-population').textContent =
   sum.toLocaleString('en-US');
